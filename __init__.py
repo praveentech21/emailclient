6c3ef818863d00815bc717e6ea6b3c5cc87705df
Ravi Kumar <ravikumar_csd@srkrec.edu.in>
2024-04-13 19:20:15 +0530

    working fine just need to triger

---
+++ __init__.py
@@ -0,0 +1,77 @@
+import base64
+from authenticate import *
+from send_resopnce import send_email
+from email_classifier import classify_emails
+from flask import Flask, redirect, request, Response
+from googleapiclient.discovery import build
+
+app = Flask(__name__)
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+@app.route('/')
+def index():
+    creds = None         
+
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    if creds and creds.valid:
+        return redirect('/emails')
+    else:
+        authorization_url, flow = authenticate()
+        return redirect(authorization_url)
+
+
+@app.route('/callback')
+def callback():
+    code = request.args.get('code')
+    flow = Flow.from_client_secrets_file(
+        'credentials.json',
+        scopes=SCOPES,
+        redirect_uri='http://localhost:5000/callback'
+    )
+    flow.fetch_token(code=code)
+    creds = flow.credentials
+
+    with open('token.json', 'w') as token:        
+        token.write(creds.to_json())
+
+    return redirect('/emails')
+
+@app.route('/emails')
+def fetch_emails():
+    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    service = build('gmail', 'v1', credentials=creds)
+
+    message_id = {'id': '18daf37abaec44fb', 'threadId': '18daf37abaec44fb'}
+
+    if message_id:
+        message = get_email(service, message_id)
+        print(message)
+        if message:
+            sender = next((header['value'] for header in message['payload']['headers'] if header['name'] == 'From'), '')
+            subject = next((header['value'] for header in message['payload']['headers'] if header['name'] == 'Subject'), '')
+            body = ''
+
+            if 'parts' in message['payload']:
+                for part in message['payload']['parts']:
+                    if part['mimeType'] == 'text/plain':
+                        body = base64.urlsafe_b64decode(part['body']['data']).decode('utf-8')
+                        break
+
+            predition = classify_emails(body, subject, sender)  
+            print(predition)
+            send_email(sender, subject, predition)  
+                            
+        return "New email received and processed."
+    else:
+        return "No new emails found."
+    
+# @app.route('/classification')
+# def classification():
+#     classify_emails(all_emails)
+#     return classify_emails(all_emails)
+
+if __name__ == '__main__':
+    app.run(port=5000)
---
+++ email_classifier.py
@@ -0,0 +1,27 @@
+import json
+import cohere
+from cohere.responses.classify import Example
+
+def classify_emails(email_body,email_subject,email_sender):
+    co = cohere.Client('Qdkqns4Mm8O9XvHZV0oO5RSoMomki7sQsYbX7Yqd')
+    
+    with open('category.json', 'r') as file:
+        examples_data = json.load(file)
+        
+    examples = [Example(example['message'], example['label']) for example in examples_data['examples']]
+
+    # Classify each email
+    if email_body:
+        classification_response = co.classify(
+            inputs=[email_body],  
+            examples=examples,
+        )
+
+        classification = classification_response[0]
+        predition = classification.prediction
+        print(predition)
+
+    return predition
+    
+if __name__ == '__main__':
+    classify_emails("Hello, I am writing to inform you that the meeting has been rescheduled to 2:00 PM tomorrow.", "Meeting rescheduled", "ravikumar_csd@srkrec.edu.in")
